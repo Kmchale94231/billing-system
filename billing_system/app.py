95b883a22e78337b73a538d7b67bde1479929ee5
Kmchale94231 <kaimchale@icloud.com>
2025-09-23 23:51:47 -0500

    Added multiple tables and functions for checkout. Need to complete checkout total function before moving on to gui implementation.

--- billing_system/app.py
+++ billing_system/app.py
@@ -1,1 +1,138 @@
+import sqlite3
+from datetime import datetime
+
+def connect():
+    con = sqlite3.connect("store.db")
+    con.execute("PRAGMA foreign_keys = ON")
+    cur = con.cursor()
+    return con
+
+# Retrieve or create a customer file 
+
+def get_or_create_customer(name, phone):
+    with connect() as con:
+        cur = con.cursor()
+
+        name = name.strip()
+
+        if not name:
+            raise ValueError("Name is required")
+
+        if phone is not None:
+            phone = phone.strip()
+            
+            if phone == "":
+                phone = None
+
+        if phone is not None:
+            cur.execute("SELECT id FROM customers WHERE name = ? and phone = ?", (name, phone))
+        else:
+            cur.execute("SELECT id FROM customers WHERE name = ? AND phone IS NULL", (name,))
+        row = cur.fetchone()
+
+        if row:
+            return row[0]
+    
+        cur.execute("INSERT INTO customers (name, phone) VALUES (?, ?)", (name, phone))
+        con.commit()
+        return cur.lastrowid
+
+def start_checkout(customer_id):
+    with connect() as con:
+        cur = con.cursor()
+
+        cur.execute("""
+            INSERT INTO orders (
+            customer_id, 
+            status, 
+            subtotal_cents, 
+            tax_cents, 
+            total_cents
+            )
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (customer_id, "OPEN", 0, 0, 0),
+        )
+
+def add_item(order_id, sku, qty): 
+    with connect() as con:
+        cur = con.cursor()
+
+        if qty <= 0:
+            raise ValueError("Quantity must be more than 1")
+        
+        cur.execute(
+            "SELECT id, name, price_cents, tax_rate FROM PRODUCTS WHERE sku = ?",
+            (sku,)
+        )
+        product = cur.fetchone()
+
+        if product is None:
+            raise ValueError("Unknown SKU")
+        product_id, price_cents, tax_rate = row
+
+        cur.execute(
+            "SELECT id, qty FROM order_item WHERE order_id = ? AND product_id = ?",
+            (order_id, product_id) 
+        )
+        item = cur.fetchone()
+
+        if item:
+            item_id, old_qty = item
+            new_qty = old_qty + item
+            new_sub = new_qty * price_cents
+            new_tax = round(new_sub * tax_rate)
+            new_total = new_sub + new_tax
+
+            cur.execute(
+                "UPDATE order_item SET qty=?,"
+                "line_subtotal_cents=?, line_tax_cents=?, line_total_cents=?," 
+                "WHERE id=?"
+                (new_qty, new_sub, new_tax, new_total, item_id)
+            )
+        else:
+
+            sub = qty * price_cents
+            tax = round(sub * tax_rate)
+            total = sub + tax
+
+            cur.execute(
+                """
+                INSERT INTO order_items (
+                    order_id,
+                    product_id,
+                    sku,
+                    name,
+                    unit_price_cents,
+                    qty,
+                    tax_rate,
+                    line_subtotal_cents,
+                    line_tax_cents,
+                    line_total_cents
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """,
+                (
+                    order_id,
+                    product_id,
+                    sku,
+                    name,
+                    unit_price_cents,
+                    qty,
+                    tax_rate,
+                    line_subtotal_cents,
+                    line_tax_cents,
+                    line_total_cents
+                )
+            )
+    
+def calculate_total():
+    """
+    Calculate and update the subtotal, tax, and total value for a single order
+    """
+
+    with connect() as con:
+        cur = con.cursor()
+
+        
 
--- billing_system/db.py
+++ billing_system/db.py
@@ -2,10 +2,100 @@ import sqlite3
 con = sqlite3.connect("store.db")
 cur = con.cursor()
 
+#Customer and product table creation
+
 cur.execute("""
-CREATE TABLE customers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    phone TEXT
-)
+    CREATE TABLE IF NOT EXISTS customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        phone TEXT
+    )
 """)
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sku TEXT,            
+        name TEXT NOT NULL,
+        price_cents INTEGER NOT NULL,
+        tax_rate FLOAT NOT NULL
+    )   
+""")
+
+# Creating the orders database
+
+con = sqlite3.connect("store.db")
+cur = con.cursor()
+
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        customer_id INTEGER NOT NULL,
+        FOREIGN KEY(customer_id) REFERENCES customers(id),
+        status TEXT NOT NULL CHECK(status IN('OPEN', 'PAID', 'VOID')),
+        subtotal_cents INTEGER NOT NULL DEFAULT 0,
+        tax_cents INTEGER NOT NULL DEFAULT 0,
+        total_cents INTEGER NOT NULL DEFAULT 0
+    )
+""")
+
+con = sqlite3.connect("store.db")
+cur = con.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS order_item (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id INTEGER NOT NULL,
+        sku TEXT,
+        name TEXT,
+        unit_price_cents INTEGER NOT NULL,
+        qty INTEGER NOT NULL CHECK(qty > 0),
+        tax_rate FLOAT NOT NULL,
+        line_subtotal_cents INTEGER NOT NULL,
+        line_tax_cents INTEGER NOT NULL,
+        line_total_cents INTEGER NOT NULL,
+        FOREIGN KEY(order_id) REFERENCES orders(id)
+    )
+""")
+con.commit()
+con.close()
+
+# Inserting products in to the database
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("Appl001", "Apple", 79, 0.07))
+con.commit()
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("Milk001", "Milk", 450, 0.07))
+con.commit()
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("BACO001", "Bacon", 600, 0.07))
+con.commit()
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("Chee001", "Cheese", 179, 0.07))
+con.commit()
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("Past001", "Pasta", 55, 0.07))
+con.commit()
+
+cur.execute("""
+INSERT INTO products (sku, name, price_cents, tax_rate)
+VALUES (?, ?, ?, ?)
+""", ("Yogu001", "Yogurt", 132, 0.07))
+con.commit()
+con.close()
